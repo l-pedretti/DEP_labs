90739bb338473032b1deb7980e5a103d6139f45f
l-pedretti <pedroleon99@hotmail.it>
2024-10-11 20:39:23 +0200

    added interfaces

--- tools/building-spring-application/src/main/java/com/nbicocchi/building_spring_application/controller/UserController.java
+++ tools/building-spring-application/src/main/java/com/nbicocchi/building_spring_application/controller/UserController.java
@@ -1,42 +1,10 @@
 package com.nbicocchi.building_spring_application.controller;
 
-import com.nbicocchi.building_spring_application.model.UserModel;
-import com.nbicocchi.building_spring_application.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import com.nbicocchi.building_spring_application.model.UserModelImpl;
 
-@RestController
-@RequestMapping("/users")
-public class UserController {
-    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
-
-    @Autowired
-    private UserService userService;
-
-    @GetMapping("/{id}")
-    public UserModel getUserById(@PathVariable Long id) {
-        LOG.info("getUserById() invoked");
-        return userService.getUserById(id);
-    }
-
-    @PostMapping
-    public UserModel createUser(@RequestBody UserModel userModel) {
-        LOG.info("createUser() invoked");
-        return userService.createUser(userModel);
-    }
-
-    @PutMapping("/{id}")
-    public UserModel updateUser(@PathVariable Long id, @RequestBody UserModel userModelDetails) {
-        LOG.info("updateUser() invoked");
-        return userService.updateUser(id, userModelDetails);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        LOG.info("deleteUser() invoked");
-        userService.deleteUser(id);
-    }
+public interface UserController {
+    UserModelImpl getUser(Long id);
+    UserModelImpl createUser(UserModelImpl userModelImpl);
+    UserModelImpl updateUser(Long id, UserModelImpl userModelImplDetails);
+    void deleteUser(Long id);
 }
-
--- tools/building-spring-application/src/main/java/com/nbicocchi/building_spring_application/service/UserService.java
+++ tools/building-spring-application/src/main/java/com/nbicocchi/building_spring_application/service/UserService.java
@@ -1,36 +1,11 @@
 package com.nbicocchi.building_spring_application.service;
 
+import com.nbicocchi.building_spring_application.model.UserModelImpl;
 
-import com.nbicocchi.building_spring_application.model.UserModel;
-import com.nbicocchi.building_spring_application.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-    @Autowired
-    private UserRepository userRepository;
-
-    public UserModel getUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
-    }
-
-    public UserModel createUser(UserModel userModel) {
-        return userRepository.save(userModel);
-    }
-
-    public UserModel updateUser(Long id, UserModel userModelDetails) {
-        UserModel userModel = userRepository.findById(id).orElse(null);
-        if (userModel != null) {
-            userModel.setName(userModelDetails.getName());
-            userModel.setEmail(userModelDetails.getEmail());
-            return userRepository.save(userModel);
-        }
-        return null;
-    }
-
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
-    }
+public interface UserService {
+    UserModelImpl getUser(Long id);
+    UserModelImpl createUser(UserModelImpl userModelImpl);
+    UserModelImpl updateUser(Long id, UserModelImpl userModelImplDetails);
+    void deleteUser(Long id);
 }
 
